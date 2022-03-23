bd5c7324e197a527e6bfa881c6dc2fe38eb16082
Stephen Lance McDaniel <stephen.l.mcdaniel@gmail.com>
2022-03-23 13:55:00 -0500

    updated users functions

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,4 @@
-const { client, getAllUsers, createUser } = require("./index");
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 // this function should call a query which drops all tables from our database
 async function dropTables() {
@@ -24,7 +24,10 @@ async function createTables() {
     CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username varchar(255) UNIQUE NOT NULL,
-        password varchar(255) NOT NULL
+        password varchar(255) NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        active BOOLEAN DEFAULT true
       );
       `);
 
@@ -42,16 +45,25 @@ async function createInitialUsers() {
     const albert = await createUser({
       username: "albert",
       password: "bertie99",
+      name: "albert",
+      location: 'Sidney, Australia',
+      active: true
     });
 
     const sandra = await createUser({
       username: "sandra",
       password: "2sandy4me",
+      name: "Just Sandra",
+      location: "Ain't tellin",
+      active: true
     });
 
     const glamgal = await createUser({
       username: "glamgal",
       password: "soglam",
+      name: "Joshua",
+      location: "Upper East Side",
+      active: true
     });
 
        console.log("Finished creating users!");
@@ -77,8 +89,16 @@ async function testDB() {
   try {
     console.log("Starting to test database...");
 
+    console.log("Calling getAllUsers")
     const users = await getAllUsers();
-    console.log("getAllUsers:", users);
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]")
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY"
+    });
+    console.log("Result:", updateUserResult);
 
     console.log("Finished database tests!");
   } catch (error) {
